78978dd3cd3f8014047b9dd5c394c86fc785302f
Jendrik Johannes <jendrik@gradle.com>
2020-04-09 01:05:35 +0200

    Clean up subproject grouping in gradle build

--- subprojects/internal-performance-testing/internal-performance-testing.gradle.kts
+++ subprojects/internal-performance-testing/internal-performance-testing.gradle.kts
@@ -14,10 +14,8 @@
  * limitations under the License.
  */
 import accessors.java
-import org.gradle.gradlebuild.unittestandcompile.ModuleType
-
 plugins {
-    `java-library`
+    gradlebuild.internal.java
     gradlebuild.classycle
 }
 
@@ -71,10 +69,6 @@ dependencies {
     implementation(testFixtures(project(":toolingApi")))
 
     runtimeOnly("com.h2database:h2:1.4.192")
-}
-
-gradlebuildJava {
-    moduleType = ModuleType.INTERNAL
 }
 
 val generatedResourcesDir = gradlebuildJava.generatedResourcesDir
